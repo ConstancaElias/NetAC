7815f7634ede66e9a6c1ce4e4a6fdae4e8cc8a6e
maria85290 <49396253+maria85290@users.noreply.github.com>
2020-04-21 15:48:03 +0100

    Upload file

--- app.py
+++ app.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 import os
 import subprocess
 import os.path
-
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 from actualizar_dicionario import actualizar_dicionario
 from leitura_ficheiro_json import leitura_ficheiro_json
+dire =  os.getcwd() 
 
-
-'''
-def execute(cmd, files):
-    subp_ret = ""
-    cmd_list = [cmd]
-    cmd_list.extend(files)
-    try:
-        subp_ret = subprocess.check_output(cmd_list)
-        """ at this point you have the output of the command in subp_ret in case you need it """
-    except Exception as e:
-        print("Failed to run subprocess. Details: " + str(e))
-    back =dict()
-    for file in files:
-        with open(file, 'r') as f:
-            info = f.read()
-            back[file] = info
-    return back
-'''
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/Uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/home")
@@ -60,31 +45,30 @@ def novapalavra():
 	output = request.args['output']
 	return render_template("novapalavra.html", output = output)
 
-@app.route("/Analise_Ficheiros",  methods=['POST', 'GET'])
-def Analise_Ficheiros():
+ALLOWED_EXTENSIONS = set(['json'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
 	if request.method == 'POST':
-		f = request.form['file']
-		string = 'static/Ficheiros_Json/' + f
-		if os.name == "nt":       
-		    encondF = "cp1252" 
-		else:
-		    encondF = "utf8"
-		f ="static/TabelaFreq_" + f+".pdf"
-		#no caso de ja existir pdf de um ficheiro, para ser mais rapido vamos so carregar o pdf sem o gerar de novo
-		if os.path.isfile(f):
-			return redirect(url_for("resultados_analise", file = f))
-		else:
-			leitura_ficheiro_json('static/dicionario_Ingles.txt', string)
-			return redirect(url_for("resultados_analise", file = f))
+		if request.file:
+			f = request.files['file']
+			f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+			leitura_ficheiro_json('static/dicionario_Ingles.txt', f)
+			return redirect(url_for("resultados_analise", filen = f))
 	else:
-		return render_template("Analise_Ficheiros.html")
+		return render_template('upload.html')
 
-@app.route('/resultados_analise')
-
+@app.route('/resultados_analise',  methods=['GET','POST'])
 def resultados_analise():
 	#output = request.args['output']
-	f = request.args['file']
+	f = request.args['filena']
 	return render_template("resultados_analise.html", file = f)
+
+
 
 
 '''
@@ -96,3 +80,4 @@ def dicionario():
 
 if __name__ == "__main__":
 	app.run()
+
